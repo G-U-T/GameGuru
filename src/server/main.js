d1b71cbac36071a5b92bd65d5660492319a39eb0
Joshua Dogma <joshuadogma@Joshuas-MacBook-Pro.local>
2023-12-19 16:00:04 -0500

    worked on console router. Almost done. Need to add prisma once complete.

--- src/server/main.js
+++ src/server/main.js
@@ -1,11 +1,18 @@
-import express from "express";
-import ViteExpress from "vite-express";
+const express = require("express"); 
+const ViteExpress =require ("vite-express");
+const consolesRouter= require('./consolesRouter')
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
+
+app.use('/consoles', consolesRouter);
+
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
