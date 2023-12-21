e4f020f64c1b87d4be3199a7e202fa665e4e3536
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-12-21 12:16:14 -0500

    Can delete review now

---
+++ prisma/migrations/20231221170622_migrate_to_add_on_delete/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_reviewsId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_gameId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_reviewsId_fkey" FOREIGN KEY ("reviewsId") REFERENCES "Review"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/server/routes/usersRouter.js
+++ src/server/routes/usersRouter.js
@@ -7,63 +7,86 @@ const usersRouter = express.Router();
 
 // Get User Profile, needed to use ID 
 usersRouter.get('/:userID', async (req, res) => {
-    const { userID } = req.params;
-  
-    try {
-      const userProfile = await prisma.user.findUnique({
-        where: { id: parseInt(userID) },
-        select: {
-          id: true,
-          username: true,
-          reviews: true,
-          comments: true,
-        },
-      });
-  
-      if (!userProfile || userProfile.length === 0) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      return res.json(userProfile);
-    } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+  const { userID } = req.params;
+
+  try {
+    const userProfile = await prisma.user.findUnique({
+      where: { id: parseInt(userID) },
+      select: {
+        id: true,
+        username: true,
+        reviews: true,
+        comments: true,
+      },
+    });
+
+    if (!userProfile || userProfile.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
-  //Update a user review 
-  usersRouter.patch('/:userID/reviews/:reviewID', async (req, res) => {
-    const { userID, reviewID } = req.params;
-  
-    try {
-      const { rating, summary } = req.body;
-  
-      // Both fields required 
-      if (!rating || !summary) {
-        return res.status(400).json({ error: 'Rating and summary are required' });
-      }
-  
-      const updatedReview = await prisma.review.update({
-        where: {
-          id: parseInt(reviewID),
-          userId: parseInt(userID),
-        },
-        data: {
-          rating: parseInt(rating),
-          summary,
-        },
-        
-        include: {
-          game: true,
-          user: true, 
-          comments: true,
-        },
-      });
-  
-      return res.json(updatedReview);
-    } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+
+    return res.json(userProfile);
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//Update a user review 
+usersRouter.patch('/:userID/reviews/:reviewID', async (req, res) => {
+  const { userID, reviewID } = req.params;
+
+  try {
+    const { rating, summary } = req.body;
+
+    // Both fields required 
+    if (!rating || !summary) {
+      return res.status(400).json({ error: 'Rating and summary are required' });
     }
-  });
+
+    const updatedReview = await prisma.review.update({
+      where: {
+        id: parseInt(reviewID),
+        userId: parseInt(userID),
+      },
+      data: {
+        rating: parseInt(rating),
+        summary,
+      },
+      
+      include: {
+        game: true,
+        user: true, 
+        comments: true,
+      },
+    });
+
+    return res.json(updatedReview);
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Delete a user review
+// TODO: needs to authenticate the user's token
+usersRouter.delete('/:userID/reviews/:reviewID', async (req, res) => {
+  const { userID, reviewID } = req.params;
+
+  try {
+    const deletedReview = await prisma.review.delete({
+      where: {
+        id: parseInt(reviewID),
+        userId: parseInt(userID),
+      }
+    })
+
+    return res.json({deleted: true, removedReview: deletedReview});
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// patch and delete for comment
 
 module.exports= usersRouter;
