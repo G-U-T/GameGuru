500703ec513b9948a2aeb1e00d1501928930ae09
SomaanIqbal <isomaan@gmail.com>
2023-12-21 13:07:44 -0800

    updated SingleGameInformation.jsx, need to create Links for single games from database for testing

---
+++ src/client/components/GameList.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const GameList = () => {
+    return (
+        <div className='gameList'>
+            
+        </div>
+    )
+}
+
+export default GameList
--- src/client/components/SingleGameInformation.jsx
+++ src/client/components/SingleGameInformation.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 
 const SingeGameInformation = () => {
-    const {gameId} = useParams();
+    const {singleGameId} = useParams();
     const [gameArray, setGameArray] = useState([]);
 
     useEffect(() => {
         const getGame = async() => {
             try {
-                const response = await fetch(`/api/console/${gameId}`)
+                const response = await fetch(`/api/games/${singleGameId}`)
                 const jsonResponse = await response.json();
                 setGameArray(result.game)
             } catch (error) {
@@ -17,6 +17,22 @@ const SingeGameInformation = () => {
         };
         getGame();
     }, [])
+
+    return (
+        <section>
+            
+            {gameArray.map((game) => {
+                return (
+                    <p>{game.title}</p>,
+                    <p>{game.release_date}</p>,
+                    <p>{game.platform}</p>,
+                    <p>{game.genre}</p>,
+                    <p>{game.description}</p>
+                    )
+                })}
+
+        </section>
+    )
 }
 
 export default SingeGameInformation
