07c41729912671b5b4c047b567eb012ad16d8797
G-U-T <147336533+G-U-T@users.noreply.github.com>
2024-01-03 15:47:48 -0500

    Changed a few things so as to not get warnings in the JS console

--- src/client/components/HomePage.jsx
+++ src/client/components/HomePage.jsx
@@ -1,43 +1,41 @@
 import '../App.css';
 
 const HomePage = () => {
-    return (
-      <body>
-      <div className="homepage-container">
-        
-      <div class="particles">
-    <div class="particle"></div>
-    <div class="particle"></div>
+  return (
+  <div className="homepage-container">
+      
+    <div className="particles">
+      <div className="particle"></div>
+      <div className="particle"></div>
     </div>
-        
-        
-        <h1>Gaming Wisdom Unleashed!</h1>
-  
-        <div className="image-container">
-          <img className="fade-in" src='/COD.webp' alt="Call of Duty GIF" />
-          <img className="fade-in" src='/spiderman.webp' alt="Spiderman GIF" />
-          <img className='fade-in' src='/mariokart.webp' alt='Mario Kart GIF'/>
-        </div>
+      
+      
+    <h1>Gaming Wisdom Unleashed!</h1>
 
-        <div className="star-rating">
-        {[1, 2, 3, 4, 5].map((star) => (
-          <span key={star} role="img" aria-label={`Star ${star}`}>
-            ⭐
-          </span>
-        ))}
-      </div>
-  
-        <div className="center-text">
-          <p className="header-text">Real Reviews, Real Insights</p>
-          <p>Join our gaming community where passion meets insights.</p>
-          <p>Discover the latest reviews, share your experiences, and connect with fellow gamers.</p>
-          <img className="fade-in" src='/gamer.jpg' alt="Gamer playing on a laptop" style={{ border: '2px solid white', width: '500px', height: 'auto' }} />
-          <img className="fade-in" src='/gamingteam.jpg' alt="team of gamers" style={{ border: '2px solid white', width: '500px', height: 'auto' }} />
-          <p className="bottom-text">Level up your gaming journey and unleash your game wisdom with Guru's!</p>
-        </div>
-      </div>
-      </body>
-    );
-  }
+    <div className="image-container">
+      <img className="fade-in" src='/COD.webp' alt="Call of Duty GIF" />
+      <img className="fade-in" src='/spiderman.webp' alt="Spiderman GIF" />
+      <img className='fade-in' src='/mariokart.webp' alt='Mario Kart GIF'/>
+    </div>
+
+    <div className="star-rating">
+    {[1, 2, 3, 4, 5].map((star) => (
+      <span key={star} role="img" aria-label={`Star ${star}`}>
+        ⭐
+      </span>
+    ))}
+    </div>
+
+    <div className="center-text">
+      <p className="header-text">Real Reviews, Real Insights</p>
+      <p>Join our gaming community where passion meets insights.</p>
+      <p>Discover the latest reviews, share your experiences, and connect with fellow gamers.</p>
+      <img className="fade-in" src='/gamer.jpg' alt="Gamer playing on a laptop" style={{ border: '2px solid white', width: '500px', height: 'auto' }} />
+      <img className="fade-in" src='/gamingteam.jpg' alt="team of gamers" style={{ border: '2px solid white', width: '500px', height: 'auto' }} />
+      <p className="bottom-text">Level up your gaming journey and unleash your game wisdom with Guru's!</p>
+    </div>
+  </div>
+  );
+}
 
 export default HomePage; 
