687ce5d9d016dd22275c81706b398e6c1208489b
G-U-T <147336533+G-U-T@users.noreply.github.com>
2024-01-04 16:24:37 -0500

    Added routes for posting/getting comments

--- src/server/api/comments.js
+++ src/server/api/comments.js
@@ -23,6 +23,55 @@ router.get('/:commentID', async(req, res) => {
   }
 });
 
+router.get('/reviews/:reviewID', jwtUtils.verifyToken, async(req, res) => {
+  const { reviewID } = req.params;
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        reviewsId: parseInt(reviewID),
+      }
+    });
+
+    return res.json(comments);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.post('/reviews/:reviewID', jwtUtils.verifyToken, async(req,res) => {
+  const { reviewID } = req.params;
+
+  try {
+    const { comment_text } = req.body;
+
+    // Comment text required 
+    if (!comment_text) {
+      return res.status(400).json({ error: 'Comment text required' });
+    }
+
+    const review = await prisma.review.findUnique({
+      where: {
+        id: parseInt(reviewID)
+      }
+    })
+    const reviewUserID = review.userId;
+
+    const newComment = await prisma.comment.create({
+      data: {
+        reviewsId: parseInt(reviewID),
+        userId: reviewUserID,
+        comment_text,
+      },
+    })
+
+    res.json({"New comment": newComment});
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // patch and delete for comment
 router.patch('/:commentID', jwtUtils.verifyToken, async (req, res) => {
   const { commentID } = req.params;
